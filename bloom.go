5d171ddad3142c46b8a63a87985c34de79aff371
Borgeuzz <mmam2407@gmail.com>
2026-01-15 23:48:21 +0100

    implemented bloom filter file Save/Load

--- bloom.go
+++ bloom.go
@@ -1,18 +1,35 @@
-package bloomfilter
+package bloom
+
+/* This file implements a classic bloom filter.
+ * It provides a New function to create a new bloom filter and the following methods:
+ * - Add: adds an element to the bloom filter
+ * - Contains: checks if an element is in the bloom filter
+ * - InsertionsCount: returns the number of elements inserted in the bloom filter
+ * - MemoryUsage: returns the memory usage of the bloom filter
+ * - SaveToFile: saves the bloom filter to a file
+ * - LoadFromFile: loads the bloom filter from a file
+ */
 
 import (
+	"encoding/binary"
 	"math"
+	"os"
 
 	"github.com/spaolacci/murmur3"
 )
 
+var hashFunc = murmur3.Sum64WithSeed
+
+// BloomFilter is the main struct that represents the classic bloom filter.
 type BloomFilter struct {
 	vec        []byte                                // byte array to store the bloom filter
 	bits       int                                   // number of bits in bloom filter array
 	hashNumber int                                   // number of hash functions
 	hashFunc   func(data []byte, seed uint32) uint64 // hash function
+	insertions int                                   // number of elements inserted in the bloom filter
 }
 
+// New creates a new bloom filter.
 func New(n int, fpr float64) *BloomFilter {
 	bytes := calculateBytes(n, fpr)
 	bits := bytes * 8
@@ -22,7 +39,8 @@ func New(n int, fpr float64) *BloomFilter {
 		vec:        make([]byte, bytes),
 		bits:       bits,
 		hashNumber: hashNumber,
-		hashFunc:   murmur3.Sum64WithSeed,
+		hashFunc:   hashFunc,
+		insertions: 0,
 	}
 }
 
@@ -74,6 +92,7 @@ func (b *BloomFilter) Add(elem string) error {
 		index := hash % uint64(b.bits)
 		b.setBit(index)
 	}
+	b.insertions++
 	return nil
 }
 
@@ -87,4 +106,62 @@ func (b *BloomFilter) Contains(elem string) bool {
 		}
 	}
 	return true
+}
+
+// InsertionsCount returns the number of elements inserted in the bloom filter.
+func (b *BloomFilter) InsertionsCount() int {
+	return b.insertions
+}
+
+// MemoryUsage returns the memory usage of the bloom filter.
+// It returns the number of bytes used by the bloom filter.
+func (b *BloomFilter) MemoryUsage() int {
+	return b.bits / 8
+}
+
+// SaveToFile saves the bloom filter to a file.
+// It save the bloom filter metadata (bits, hash number, insertions) and byte array to the file.
+func (b *BloomFilter) SaveToFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// write bytes to save bloom filter metadata and byte array
+	// use int32 to ensure consistent size on all platforms
+	binary.Write(file, binary.LittleEndian, int32(b.bits))
+	binary.Write(file, binary.LittleEndian, int32(b.hashNumber))
+	binary.Write(file, binary.LittleEndian, int32(b.insertions))
+
+	// write byte array to file
+	_, err = file.Write(b.vec)
+	return err
+}
+
+// LoadFromFile loads the bloom filter from a file.
+// It loads the bloom filter metadata (bits, hash number, insertions) and byte array from the file.
+func LoadFromFile(filename string) (*BloomFilter, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// read bytes from file using int32 to match SaveToFile
+	b := &BloomFilter{}
+	var bits, hashNumber, insertions int32
+	binary.Read(file, binary.LittleEndian, &bits)
+	binary.Read(file, binary.LittleEndian, &hashNumber)
+	binary.Read(file, binary.LittleEndian, &insertions)
+
+	b.bits = int(bits)
+	b.hashNumber = int(hashNumber)
+	b.insertions = int(insertions)
+	b.hashFunc = hashFunc
+
+	// read byte array from file
+	b.vec = make([]byte, (b.bits+7)/8)
+	_, err = file.Read(b.vec)
+	return b, err
 }
--- bloom_test.go
+++ bloom_test.go
@@ -1,4 +1,4 @@
-package bloomfilter
+package bloom
 
 import (
 	"fmt"
@@ -38,4 +38,30 @@ func TestBloomFilterFalsePositiveRate(t *testing.T) {
 	falsePositiveRate := float64(falsePositiveCount) / 100000
 	t.Logf("false positive rate: %f", falsePositiveRate)
 	require.Less(t, falsePositiveRate, 0.01)
+}
+
+func TestBloomFilterSaveAndLoadFromFile(t *testing.T) {
+	bf := New(1000, 0.01)
+	bf.Add("hello")
+	bf.Add("world")
+	bf.SaveToFile("test.bf")
+
+	bf2, err := LoadFromFile("test.bf")
+	if err != nil {
+		t.Errorf("failed to load bloom filter from file: %v", err)
+	}
+
+	// check if bloom metadata are the same
+	require.Equal(t, bf.hashNumber, bf2.hashNumber)
+	require.Equal(t, bf.bits, bf2.bits)
+	require.Equal(t, bf.vec, bf2.vec)
+
+	// check if bloom filter is the same
+	require.Equal(t, bf.MemoryUsage(), bf2.MemoryUsage())
+	require.Equal(t, bf.InsertionsCount(), bf2.InsertionsCount())
+
+	// check if bloom filter contains the same elements
+	require.True(t, bf2.Contains("hello"))
+	require.True(t, bf2.Contains("world"))
+	require.False(t, bf2.Contains("foo"))
 }
